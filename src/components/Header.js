ce28dd8813040adb9261b92af5e6d32b0bf24ef8
Deokgwan Kim <deokgwan.kim@big6media.com>
2021-05-27 22:44:21 -0500

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,6 @@ class Header extends React.Component {
             <div className="header">
             <Navbar className="navbar navbar-expand-md custom-nav fixed-top">
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                {/* <button class="navbar-toggler collapsed btn menu-button" id="menu" type="button" data-toggle="collapse" data-target="#menuMobileButton" aria-controls="#menuMobileButton" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="sr-only">Toggle navigation</span>
-                    <i class="fas fa-bars"></i>
-                </button> */}
                 <Navbar.Collapse id="navbarScroll">
                     <Nav className="mr-auto">
                         <Nav.Link href="#about">About</Nav.Link>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -16,7 +16,7 @@ const Hero = () => (
                 <h2 className="h1">HELLO, WORLD!</h2>
                 <h1>A Creative Full Stack Developer in Kansas</h1>
                 <p>Interested in Working Together? Check out my Resume</p>
-                <a className="btn btn-primary">See My Resume</a>
+                <a className="btn btn-primary" href="https://drive.google.com/file/d/1QbfL_JGXB7TkNYpbiJ8r08T2HCraN1t7/view?usp=sharing" target="_blank">DOWNLOAD CV</a>
             </div>
         </div>
     </div>
