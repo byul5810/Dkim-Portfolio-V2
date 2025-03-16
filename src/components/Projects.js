cef74b8f09d0153534925f19967a7ed41c7d5c29
Deokgwan Kim <deokgwankim@Deokgwans-MBP.lan>
2025-03-16 17:46:36 -0500

    work updates

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import ProjectCarousel from './Project_Carousel'
 import MobileProjectCarousel from './Mobile_Project_Carousel'
-import Arduino from '../assets/arduino.jpg'
+import Angular from '../assets/angular.png'
+import Cross from '../assets/cross.png'
 import Ecommerce from '../assets/ecommerce.jpeg'
 import '../style/projects.scss';
 
@@ -16,9 +17,7 @@ class Projects extends Component {
               </div>
               <div className="row ">
                 <div className="card col-md-5 px-0">
-                  <a href="https://github.com/byul5810/Temp-Humidity-Arduino-with-Android" target="_blank=">
-                    <img className="project-img" src={Arduino} alt="img"></img>
-                  </a>
+                  <img className="project-img" src={Angular} alt="img"></img>
                 </div>
                 <div className="project-content offset-md-1 col-md-6">
                     <h2>AngularJS to Angular Migration</h2>
@@ -26,35 +25,29 @@ class Projects extends Component {
                     </p>
                 </div>
               </div>
-              <div className="row mt-5 pt-5">
+              <div class="row mt-5 pt-5">
                 <div className="project-content col-md-6 d-md-block d-none">
-                    <h2>Mobile Friendly + SEO optimization</h2>
-                    <p>I always consider mobile and website SEO optimization before coding to explore visual and usability decisions
-                      to provide the most efficient-quality website with the best SEO practice and mobile optimization for clients.
-                      I have good website SEO knowledge and using that making their website get a high ranking on google search.
-                    </p>
+                  <h2>Cross-Technology Integration</h2>
+                  <p> I am managing several projects simultaneously, working across various repositories and technologies. I handle multiple, including Angular, AngularJS, VueJS, and Java. I take on complex tasks that require juggling multiple priorities and platforms, ensuring that each project runs smoothly and efficiently. My ability to seamlessly integrate different technologies and drive progress across various initiatives has consistently led to improved performance and results.</p>
                 </div>
-                <div className="card mobile-project offset-md-2 col-md-3 px-0">
-                <MobileProjectCarousel />
+                <div className="card offset-md-1 col-md-5 px-0">
+                  <img className="project-img" src={Cross} alt="img"></img>
                 </div>
                 <div className="project-content mobile col-md-6 d-md-none d-block">
+                  <h2>Cross-Technology Integration</h2>
+                  <p> I am managing several projects simultaneously, working across various repositories and technologies. I handle multiple, including Angular, AngularJS, VueJS, and Java. I take on complex tasks that require juggling multiple priorities and platforms, ensuring that each project runs smoothly and efficiently. My ability to seamlessly integrate different technologies and drive progress across various initiatives has consistently led to improved performance and results.</p>
+                </div>
+              </div>
+              <div className="row mt-5 pt-5">
+                <div className="card mobile-project offset-md-1 col-md-3 px-0">
+                <MobileProjectCarousel />
+                </div>
+                <div className="project-content offset-md-2 col-md-6">
                     <h2>Mobile Friendly + SEO optimization</h2>
                     <p>I always consider mobile and website SEO optimization before coding to explore visual and usability decisions
                       to provide the most efficient-quality website with the best SEO practice and mobile optimization for clients.
                       I have good website SEO knowledge and using that making their website get a high ranking on google search.
                     </p>
-                </div>
-              </div>
-              <div class="row mt-5 pt-5">
-                <div className="card col-md-5 px-0">
-                <ProjectCarousel />
-                </div>
-                <div className="project-content offset-md-1 col-md-6">
-                    <h2>Website Template Build</h2>
-                    <p>Tons of website build experience with an excellent sense of design. I provide the most high-quality website.
-                      I prioritize writing well-organized code and using modern programming methods to create a high-quality foundation.
-                      I write automated tests to make sure each code is as bug-free and high quality as possible.
-                      I always try to use an efficient and creative solution</p>
                 </div>
               </div>
             </div>
